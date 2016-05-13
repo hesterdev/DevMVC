f10e234a2c7a6a5ae5a08a7e5247d8715afdb5c5
hesterdev <940356013@qq.com>
2016-05-13 17:47:44 +0800

    start autoproperty

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index() {
+            return "Navigate to a URL to show an example";
+        }
+    }
+}
