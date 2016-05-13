b7e5ae0728f99d39bc2cfdab12a709457b81aa6f
hesterdev <940356013@qq.com>
2016-05-13 21:17:00 +0800

    Extension Method

--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace LanguageFeatures.Models {
-    public class Product {
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
         private string name;
 
-        public string Name {
-            get { return name; }
-            set { name = value; }
+        public int ProductID { get; set; }
+        public string Name
+        {
+            get
+            {
+                return ProductID + name;
+            }
+            set
+            {
+                name = value;
+            }
         }
+        public string Description { get; set; }
+        public Decimal Price { get; set; }
+        public string Category { get; set; }
     }
 }
