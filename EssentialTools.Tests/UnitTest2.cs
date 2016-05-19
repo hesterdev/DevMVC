19789315a2c4108ccd0c6f87a922776045d1b9c0
hesterdev <940356013@qq.com>
2016-05-19 21:22:59 +0800

    Chapter 6 over

--- EssentialTools.Tests/UnitTest2.cs
+++ EssentialTools.Tests/UnitTest2.cs
@@ -23,9 +23,15 @@ namespace EssentialTools.Tests
             Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
             mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>()))
                 .Returns<decimal>(total => total);
+            var target = new LinqValueCalculator(mock.Object);
 
+            // act
+            var result = target.ValueProducts(products);
 
+            // assert
+            Assert.AreEqual(products.Sum(e => e.Price), result);
 
+            /*
             var discounter = new MinumumDiscountHelper();
             var target = new LinqValueCalculator(discounter);
             //var goalTotal = discounter.ApplyDiscount(products.Sum(e => e.Price));
@@ -36,6 +42,42 @@ namespace EssentialTools.Tests
 
             // assert 
             Assert.AreEqual(goalTotal, result);
+            */
+        }
+
+        private Product[] CreateProduct(decimal value)
+        {
+            return new[] { new Product { Price = value } };
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void Pass_Through_Variable_Discounts()
+        {
+            // arrange
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>())).Returns<decimal>(total => total);
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v == 0))).Throws<ArgumentOutOfRangeException>();
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v > 100))).Returns<decimal>(total => (total * 0.9M));
+            mock.Setup(m => m.ApplyDiscount(It.IsInRange<decimal>(10, 100, Range.Inclusive))).Returns<decimal>(total => total - 5);
+            var target = new LinqValueCalculator(mock.Object);
+
+            // act
+            decimal FiveDollarDiscount = target.ValueProducts(CreateProduct(5));
+            decimal TenDollarDiscount = target.ValueProducts(CreateProduct(10));
+            decimal FiftyDollarDiscount = target.ValueProducts(CreateProduct(50));
+            decimal HundredDollarDiscount = target.ValueProducts(CreateProduct(100));
+            decimal FiveHundredDollarDiscount = target.ValueProducts(CreateProduct(500));
+
+            // assert
+            Assert.AreEqual(5, FiveDollarDiscount, "$5 fail");
+            Assert.AreEqual(5, TenDollarDiscount, "$10 fail");
+            Assert.AreEqual(45, FiftyDollarDiscount, "$50 fail");
+            Assert.AreEqual(95, HundredDollarDiscount, "$100 fail");
+            Assert.AreEqual(450, FiveHundredDollarDiscount, "$500 fail");
+            target.ValueProducts(CreateProduct(0));
+
+
         }
     }
 }
---
+++ PrimeNum.Test/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace PrimeNum.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_PrimeNum_IsMath()
+        {
+            // arrange
+            PrmNum target = new PrmNum();
+
+            // set 
+            bool ret1 = target.IsMath(1);
+            bool ret2 = target.IsMath(2);
+            bool ret3 = target.IsMath(3);
+            bool ret4 = target.IsMath(4);
+
+            bool ret12 = target.IsMath(12);
+
+            bool ret73 = target.IsMath(73);
+
+            // assert
+            Assert.AreEqual(false, ret1);
+            Assert.AreEqual(true, ret2);
+            Assert.AreEqual(true, ret3);
+            Assert.AreEqual(false, ret4);
+            Assert.AreEqual(false, ret12);
+            Assert.AreEqual(true, ret73);
+        }
+    }
+}
